16208c9a6ae3fc7c3c59f8ea7da93ada434da35a
Camilo García <gcgarciab@slabcode.com>
2018-07-20 13:37:41 -0500

    Let the discussion begin - Form to put info in threads

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,6 +2,20 @@
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
     <PostList :posts="posts"/>
+    <form @submit.prevent="addPost">
+      <div class="form-group">
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          v-model="newPostText"
+        ></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -18,7 +32,8 @@
 
     data () {
       return {
-        thread: sourceData.threads[this.id]
+        thread: sourceData.threads[this.id],
+        newPostText: ''
       }
     },
 
@@ -33,6 +48,27 @@
 
     components: {
       PostList
+    },
+
+    methods: {
+      addPost () {
+        // Add post to the Post object
+        const postId = 'greatPost' + Math.random()
+        const post = {
+          text: this.newPostText,
+          publishedAt: Math.floor(Date.now() / 1000),
+          threadId: this.id,
+          userId: 'jUjmgCurRRdzayqbRMO7aTG9X1G2',
+          '.key': postId
+        }
+        // Vue set method to reactive changes
+        // set(Object, propertyName, value)     [ Object -> Objeto al que queremos agregar la nueva propiedad, propertyName -> Nombre de la propiedad a agregar, Value -> Valor de la propiedad ]
+
+        this.$set(sourceData.posts, postId, post)
+        this.$set(this.thread.posts, postId, postId)
+        this.$set(sourceData.users[post.userId].posts, postId, postId)
+        this.newPostText = ''
+      }
     }
   }
 </script>
