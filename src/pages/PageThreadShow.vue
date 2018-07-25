31f5697f56890c7a0d37690383fad36b525b4697
Camilo García <gcgarciab@slabcode.com>
2018-07-24 22:35:50 -0500

    Migrating to Vuex

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -11,7 +11,6 @@
 </template>
 
 <script>
-  import sourceData from '@/data'
   import PostList from '@/components/PostList'
   import PostEditor from '@/components/PostEditor'
   export default {
@@ -24,7 +23,7 @@
 
     data () {
       return {
-        thread: sourceData.threads[this.id],
+        thread: this.$store.state.threads[this.id],
         newPostText: ''
       }
     },
@@ -33,7 +32,7 @@
       posts () {
         const postIds = Object.values(this.thread.posts)
         // Convert object y array && Filter in Posts by id
-        return Object.values(sourceData.posts)
+        return Object.values(this.$store.state.posts)
           .filter(post => postIds.includes(post['.key']))
       }
     },
@@ -49,9 +48,9 @@
         // set(Object, propertyName, value)     [ Object -> Objeto al que queremos agregar la nueva propiedad, propertyName -> Nombre de la propiedad a agregar, Value -> Valor de la propiedad ]
         const postId = post['.key']
 
-        this.$set(sourceData.posts, postId, post)
+        this.$set(this.$store.state.posts, postId, post)
         this.$set(this.thread.posts, postId, postId)
-        this.$set(sourceData.users[post.userId].posts, postId, postId)
+        this.$set(this.$store.state.users[post.userId].posts, postId, postId)
       }
     }
   }
