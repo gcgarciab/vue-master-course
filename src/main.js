39d23322194a7c96115f40bd62b0f7a8486c647a
Camilo García <gcgarciab@slabcode.com>
2018-08-06 16:34:30 -0500

    Install Firebase Realtime Database

--- src/main.js
+++ src/main.js
@@ -5,9 +5,22 @@ import App from './App'
 import router from './router'
 import store from '@/store'
 import AppDate from '@/components/AppDate'
+import firebase from 'firebase'
 
 Vue.component('AppDate', AppDate)
 Vue.config.productionTip = false
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyAZ5WDdWHTAcCYzNioW95Kb6p1fg3eT7ww',
+  authDomain: 'vue-school-forum-8dbd4.firebaseapp.com',
+  databaseURL: 'https://vue-school-forum-8dbd4.firebaseio.com',
+  projectId: 'vue-school-forum-8dbd4',
+  storageBucket: 'vue-school-forum-8dbd4.appspot.com',
+  messagingSenderId: '199481282060'
+}
+
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
